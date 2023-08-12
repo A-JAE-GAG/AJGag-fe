140f3527a68331fd1e01e318be56fd431d32363b
Nidurolak <jaewoon08@gmail.com>
2023-08-12 23:25:16 +0900

    feat : 개그 리스트 기본 프레임 잡음

--- src/pages/GagList.tsx
+++ src/pages/GagList.tsx
@@ -12,15 +12,17 @@ function GagList() {
   const { handleSubmit, control, watch } = useForm<FormData>();
   const input1Value = watch('input1', '');
   const input2Value = watch('input2', '');
+  const [currentPageNum, setCurrentPageNum] = useState<number>(1)
+  const [MaxPageNum, setMaxPageNum] = useState<number>(10)
 
-    const [selectedOption, setSelectedOption] = useState('최신'); // Initial selection
-  
-    const handleButtonClick = (value : string) => {
-      setSelectedOption(value);
-      console.log(value); // Log the selected value to the console
-    };
-  
-  
+  const [selectedOption, setSelectedOption] = useState('최신'); // Initial selection
+
+  const handleButtonClick = (value: string) => {
+    setSelectedOption(value);
+    console.log(value); // Log the selected value to the console
+  };
+
+
   const onSubmit = (data: FormData) => {
     console.log('Form Data:', data);
   };
@@ -28,81 +30,99 @@ function GagList() {
   return (<BackgroundBox>
     <UpsideBox>
       <>
-    <div>
-      <button
-        onClick={() => handleButtonClick('최신')}
-        style={{ fontWeight: selectedOption === '최신' ? 'bold' : 'normal' }}
-      >
-        최신
-      </button>
-      <button
-        onClick={() => handleButtonClick('주간')}
-        style={{ fontWeight: selectedOption === '주간' ? 'bold' : 'normal' }}
-      >
-        주간
-      </button>
-      <button
-        onClick={() => handleButtonClick('인기')}
-        style={{ fontWeight: selectedOption === '인기' ? 'bold' : 'normal' }}
-      >
-        인기
-      </button>
-    </div>
+        <div>
+          <RadioButton
+            onClick={() => handleButtonClick('최신')}
+            style={{ fontWeight: selectedOption === '최신' ? 'bold' : 'normal' }}
+          >
+            최신개그
+          </RadioButton>
+          <span>|</span>
+          <RadioButton
+            onClick={() => handleButtonClick('주간')}
+            style={{ fontWeight: selectedOption === '주간' ? 'bold' : 'normal' }}
+          >
+            주간개그
+          </RadioButton>
+          <span>|</span>
+          <RadioButton
+            onClick={() => handleButtonClick('인기')}
+            style={{ fontWeight: selectedOption === '인기' ? 'bold' : 'normal' }}
+          >
+            인기개그
+          </RadioButton>
+        </div>
       </>
     </UpsideBox>
     <GagOverlay></GagOverlay>
     <ListBox>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
-    <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
+      <GagListComp></GagListComp>
     </ListBox>
+    <PageBox>
+
+    </PageBox>
   </BackgroundBox>
   );
 }
 
-  
-  export default GagList;
 
-  const UpsideBox = styled.div`
+export default GagList;
+
+const PageBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`
+
+const RadioButton = styled.button`
+  border: none;
+  background: none;
+  font-size: 35px;
+  color: gray;`
+const UpsideBox = styled.div`
   width:80%;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  `
+  span{
+    font-size: 35px;
+  }`
 
-  const GagOverlay = styled.div`
+const GagOverlay = styled.div`
   width:100%;
   max-width: 1540px;
   height: 5px;
   background-color: rgba(0, 0, 0, 1); /* Overlay color */
-  z-index: 1; /* Position above GagBox */
-`;
+  z-index: 1; /* Position above GagBox */`
 
-  const ListBox = styled.div`
+const ListBox = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   gap: 10px;
   margin-bottom: 35px;
   `
 
-  const BackgroundBox = styled.div`
+const BackgroundBox = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    gap: 10px;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
   `
