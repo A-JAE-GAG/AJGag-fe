09bd092c8cb4b94031b99a508c99da872a889a1e
Nidurolak <jaewoon08@gmail.com>
2023-08-23 16:20:42 +0900

    feat : 무작위 개그 Id 불러오기 기능 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,22 +5,36 @@ import LoginModal from '../components/LoginModal';
 import { useRecoilValue } from "recoil";
 import { gagModalState, loginState } from '../store/atom';
 import GagModal from '../components/GagModal';
+import { getMainGag } from '../utils/api/api';
+import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 
 
 function Main (){
+
+  const navigate = useNavigate()
+  const [contentlist, setContentlist] = useState<any>([])
+  const { isLoading } = useQuery(["getMain",],
+  getMainGag,
+{
+  onSuccess:({ data })=>{
+    setContentlist(data.data)
+    console.log(data.data)
+    console.log(data)
+  }
+})
       return (<>
-      
       <Maincontainer>
         <MainTitleContainer>
         <h1>ㅇㅈ개그</h1>
         <h2>당신의 개그는 아재? 인정?</h2>
         </MainTitleContainer>
         <SubTitleContainer>
-        <h3>물고기 중 가장 똑똑한 물고기는?</h3>
-        <h4>대답할 자신있다면 클릭!</h4>
+        <h3>{contentlist.title}</h3>
+        <h4 onClick={()=>navigate(`/GagDetail/?id=${contentlist.gagId}`)}>대답할 자신있다면 클릭!</h4>
         </SubTitleContainer>
         <ButtonContainer>
-        <MainButton name = {"개그 맞춰보기"} link = {"/GagDetail"}></MainButton>
+        <MainButton name = {"개그 맞춰보기"} link = {`/GagDetail/?id=${contentlist.anotherGagId}`}></MainButton>
         <MainButton name = {"개그 올려보기"} link = {""}></MainButton>
         <MainButton name = {"개그 전부보기"} link = {"/GagList"}></MainButton>
         </ButtonContainer>
@@ -75,6 +89,7 @@ function Main (){
     font-size: 25px;
   }
   h4{
+    cursor: pointer;
     margin: 0px;
     overflow: hidden;
     color: gray;
